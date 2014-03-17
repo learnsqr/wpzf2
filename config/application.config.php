540fe13626cca99b52056acc3ed83a28204ea25e
Agustin Calderon <agustincl@gmail.com>
2014-03-17 16:52:05 +0100

    Menu and Breadcrumb added

--- config/application.config.php
+++ config/application.config.php
@@ -9,7 +9,8 @@ return array(
     'modules' => array(
         'Application',
         'Simplemodule',
-        'Album','Checklist'
+        'Album',
+    	'Checklist'
     ),
     'module_listener_options' => array(
         'module_paths' => array(
