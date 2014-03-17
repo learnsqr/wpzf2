46a4f667a670994f5436152c03ebe8d9158d2b99
Agustin Calderon <agustincl@gmail.com>
2014-03-17 18:06:19 +0100

    menu.config added

---
+++ module/Application/config/menu.config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    array(
+    	'label' => 'Home',
+    	'route' => 'home',
+    ),
+	
+	array(
+			'label' => 'Task',
+			'route' => 'task',
+	),
+);
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -60,7 +60,15 @@ return array(
         'aliases' => array(
             'translator' => 'MvcTranslator',
         ),
+        'factories' => array(
+        		'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory', 
+        ),
     ),
+    
+    'navigation' => array(
+    		'default' => include('menu.config.php')
+    ),
+    
     'translator' => array(
         'locale' => 'en_US',
         'translation_file_patterns' => array(
