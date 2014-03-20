8a738cfe6055334e30cb46305e109cf707a39848
Agustin Calderon <agustincl@gmail.com>
2014-03-20 13:30:24 +0100

    configuration database changed

--- config/autoload/database.global.php
+++ config/autoload/database.global.php
@@ -10,9 +10,9 @@
  */
 return array(
 		'service_manager' => array(
-			'factories' => array(
-					'Zend\Db\Adapter\Adapter'=> 'Zend\Db\Adapter\AdapterServiceFactory',						
-			),				
+// 			'factories' => array(
+// 					'Zend\Db\Adapter\Adapter'=> 'Zend\Db\Adapter\AdapterServiceFactory',						
+// 			),				
 			'abstract_factories' => array(
 					'Zend\Db\Adapter\AdapterAbstractServiceFactory',
 			),
